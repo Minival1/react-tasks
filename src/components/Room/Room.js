2541916b594d55bbeeb8e7328bba0ccf10b70110
Valeriy <mrminival2@mail.ru>
2021-10-14 01:39:24 +0300

    настроен timepicker

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -2,35 +2,36 @@ import React from "react";
 import {Button, Form} from 'antd';
 import styles from "./Room.module.css";
 import { TimePicker } from "@progress/kendo-react-dateinputs";
+import { format, isWithinInterval } from 'date-fns'
 
 const data = [
     {
-        title: "Олимпиада с 8:30 до 12:00",
+        title: "Мероприятие №1 с 8:30 до 12:00",
         id: 1,
         children: [
             {
-                title: 'Танцы',
+                title: 'Аудитория №1',
                 time: 'от 8:00 до 8:30',
                 id: 1
             },
             {
-                title: 'Стрельба',
+                title: 'Аудитория №2',
                 time: 'от 8:30 до 9:00',
                 id: 2
             },
         ]
     },
     {
-        title: "Концерт с 12:00 до 15:00",
+        title: "Мероприятие №2 с 12:00 до 15:00",
         id: 2,
         children: [
             {
-                title: 'Хор',
+                title: 'Аудитория №40',
                 time: 'от 12:00 до 12:30',
                 id: 1
             },
             {
-                title: 'Танцы',
+                title: 'Аудитория №43',
                 time: 'от 12:30 до 15:00',
                 id: 2
             },
@@ -40,26 +41,42 @@ const data = [
 
 const Room = () => {
 
-    const format = "HH:mm"
+    const time = {
+        format: "HH:mm",
+        min: new Date(null, null, null, 8, 0),
+        max: new Date(null, null, null, 18, 0),
+        steps: {
+            minute: 30
+        }
+    }
 
     const onFinish = (values) => {
-        console.log(values)
+        const formatStartDate = format(values.start_time, time.format)
+        const formatEndDate = format(values.end_time, time.format)
+
+        console.log(isWithinInterval(values.start_time, {
+            start: values.start_time, end: values.end_time
+        }))
+
     }
 
     return (
         <div>
-            <Form name="test" onFinish={onFinish}>
+            <Form initialValues={{
+                ["start_time"]: time.min,
+                ["end_time"]: time.max
+            }} name="test" onFinish={onFinish}>
                 <div className={styles.timepicker}>
                     <div>
                         <p>Начало мероприятия</p>
                         <Form.Item name="start_time">
-                            <TimePicker format={format} minuteStep={30} secondStep={10}/>
+                            <TimePicker placeholder="Select time" steps={time.steps} defaultValue={time.min} min={time.min} max={time.max} format={time.format} />
                         </Form.Item>
                     </div>
                     <div>
                         <p>Окончание мероприятия</p>
                         <Form.Item name="end_time">
-                            <TimePicker format={format} minuteStep={30} secondStep={10}/>
+                            <TimePicker placeholder="Select time" steps={time.steps} defaultValue={time.max} min={time.min} max={time.max} format={time.format} />
                         </Form.Item>
                     </div>
                     <Button type="primary" htmlType="submit">Подобрать</Button>
@@ -90,7 +107,6 @@ const Room = () => {
             </table>
         </div>
     )
-    // return <Table columns={columns} dataSource={data} scroll={{x: "100vw"}} />
 }
 
 export default Room
